fc2d26c30236d04ecb4ed611d17ee6baf0d1de42
akashr206 <akashr6514@gmail.com>
2024-10-31 16:54:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -124,5 +124,8 @@ app.post('/connect', isAuthenticated, async (req, res) => {
     }
   }
 })
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-module.exports = app;
